882028b6d54201d5154ec0f30cd7cf63ac5c87f5
jstotero <asterione@gmail.com>
2012-03-27 00:10:22 +0200

    added service scripts : AnyKernel - compilation parameters - create flashable zip script

---
+++ create_zip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir -p releases
+now=$(date +"%Y%m%d_%H%M")
+cp -f arch/arm/boot/zImage AnyKernel/kernel/
+cp -f drivers/net/wireless/libra/librasdioif.ko AnyKernel/system/lib/modules/
+cp -f drivers/net/wireless/bcm4330/wireless.ko AnyKernel/system/lib/modules/
+cd AnyKernel
+zip -r ./../cucciolone-${now}.zip *
+cd ..
+mv cucciolone-${now}.zip releases/
+
---
+++ setenv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-androideabi-
