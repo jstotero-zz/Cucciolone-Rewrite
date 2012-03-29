33041fd6d3de3e50412627ebe52cde7d98a9b2da
jstotero <asterione@gmail.com>
2012-03-29 13:02:01 +0200

    config : added ext3/ext4 full support, added cifs (embedded), added tun.ko -- smartassv2 as default, removed msm_test modules, removed mtd_test modules and something more?

--- create_zip.sh
+++ create_zip.sh
@@ -4,6 +4,7 @@ now=$(date +"%Y%m%d_%H%M")
 cp -f arch/arm/boot/zImage AnyKernel/kernel/
 cp -f drivers/net/wireless/libra/librasdioif.ko AnyKernel/system/lib/modules/
 cp -f drivers/net/wireless/bcm4330/wireless.ko AnyKernel/system/lib/modules/
+cp -f drivers/net/tun.ko AnyKernel/system/lib/modules/
 cd AnyKernel
 zip -r ./../cucciolone-${now}.zip *
 cd ..
