3c59268f8cce6ae059972b5ea5f3350ec90fb13b
jstotero <asterione@gmail.com>
2012-04-01 22:07:01 +0200

    various patches from thachtunganh / lge-kernel-thunderg
    CSW USB Performance hacks,
    Added 2 Governors: smartass2 and minmax,
    Added TINY_PREEMPT_RCU, RCU tweaks.

--- drivers/usb/gadget/storage_common.c
+++ drivers/usb/gadget/storage_common.c
@@ -297,7 +297,6 @@ static struct fsg_lun *fsg_lun_from_dev(struct device *dev)
 #define FSG_NUM_BUFFERS    2
 #endif
 
-
 /* Default size of buffer length. */
 #define FSG_BUFLEN	((u32)16384)
 
--- kernel/sched.c
+++ kernel/sched.c
@@ -9361,3 +9361,4 @@ void synchronize_sched_expedited(void)
 EXPORT_SYMBOL_GPL(synchronize_sched_expedited);
 
 #endif /* #else #ifndef CONFIG_SMP */
+EXPORT_SYMBOL_GPL(nr_running);
